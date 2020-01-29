cae472cb5d27fd03294adabe4a9a5f2704bc01b6
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-01-29 12:23:08 -0500

    added key.classlist.add for playing class

--- JS/script.js
+++ JS/script.js
@@ -4,5 +4,5 @@ window.addEventListener('keydown', function (e) {
     if (!audio) return;
     audio.currentTime = 0;
     audio.play();
-    console.log(key);
+    key.classList.add("playing");
 });
